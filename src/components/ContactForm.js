cf381ca89b8035e9ac8490961bb5c23aeaf78a7b
Jeremy C <jchow@Jeremys-MacBook-Pro.local>
2019-10-28 16:55:07 -0700

    Moving sg import to index.js from contactform

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -15,18 +15,7 @@ class ContactForm extends Component{
         const email = document.getElementById('email').value
         const message = document.getElementById('message').value
 
-        const sgMail = require('@sendgrid/mail');
-        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-        const msg = {
-          to: 'jeremyrchow@gmail.com',
-          from: 'jeremyrchow@gmail.com',
-          subject: 'Sending with Twilio SendGrid is Fun',
-          text: 'and easy to do anywhere, even with Node.js',
-          html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-        };
-
-        console.log(msg)
-        sgMail.send(msg);
+        
         // const data = new FormData(e.target);
 
         // fetch('/api/form-submit-url', {
--- src/index.js
+++ src/index.js
@@ -9,4 +9,18 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
+const sgMail = require('@sendgrid/mail');
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+          to: 'jeremyrchow@gmail.com',
+          from: 'jeremyrchow@gmail.com',
+          subject: 'Sending with Twilio SendGrid is Fun',
+          text: 'and easy to do anywhere, even with Node.js',
+          html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+        };
+
+console.log(msg)
+sgMail.send(msg);
+
 serviceWorker.unregister();
